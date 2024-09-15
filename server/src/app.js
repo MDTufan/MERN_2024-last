861732626be82873fdaaa3a1c0667347d86ea56b
Tufan <alitufan702@gmail.com>
2024-09-14 19:30:00 -0700

    error handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express= require("express");
 const app = express();
+const createError = require('http-errors')
 
 const morgan = require('morgan')
 
@@ -10,6 +11,27 @@ app.use(morgan("dev"));
 app.get("/",(req,res)=>{
  res.send("welcone to the server");
 });
+
+
+
+app.use((req,res,next)=>{
+
+    next(createError(404,"Route not Found"));
+ 
+});
+
+
+app.use((err, req, res, next) => {
+    // console.error(err.stack)
+    // res.status(500).send('Something broke!')
+
+    return res.status(err.status || 500).json({
+        sucess:false,
+        message:err.message
+    })
+  })
+
+
 
 
 module.exports={app};
